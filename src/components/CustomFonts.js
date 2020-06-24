2e3f2017b21c35ee2d63653d977bae8a4780acab
Andres Bedoya <mortiis.angel@gmail.com>
2020-06-24 18:20:19 -0500

    google verification

--- src/components/CustomFonts.js
+++ src/components/CustomFonts.js
@@ -7,6 +7,7 @@ const CustomFonts = () => (
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap"
     />
+    <meta name="google-site-verification" content="HYE6NJXs0a72sfd3IEvWA9BLt4EUoTHbfrKLxL4cuVM" />
   </Helmet>
 )
 
