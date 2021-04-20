9002d369ca5a7e1e2423d038d19dbe165a3acc6a
Andres Bedoya <mortiis.angel@gmail.com>
2021-04-20 18:33:17 -0500

    updating header

--- src/components/header.js
+++ src/components/header.js
@@ -8,24 +8,24 @@ const Header = ({ location, siteTitle }) => {
   return (
     <header className="flex items-center p-4 lg:px-8 justify-between">
       <div>
-        { location.pathname === rootPath
+        {location.pathname === rootPath
           ? <h1>
-              <Link to="/">
-                <span className="border-solid border-2 border-black font-extrabold h-16 inline-flex items-center justify-center mr-4 rounded-full text-center w-16">VE</span>
-                { siteTitle }
-              </Link>
-            </h1> 
+            <Link to="/">
+              <span className="border-solid border-2 border-black font-extrabold h-16 inline-flex items-center justify-center mr-4 rounded-full text-center w-16">VE</span>
+              {siteTitle}
+            </Link>
+          </h1>
           : <h2>
-              <Link to="/">
-                <span className="border-solid border-2 border-black font-extrabold h-16 inline-flex items-center justify-center mr-4 rounded-full text-center w-16">VE</span>
-                { siteTitle }
-              </Link>
-            </h2>
+            <Link to="/">
+              <span className="border-solid border-2 border-black font-extrabold h-16 inline-flex items-center justify-center mr-4 rounded-full text-center w-16">VE</span>
+              {siteTitle}
+            </Link>
+          </h2>
         }
       </div>
       <a
         className="bg-blue px-4 py-1 rounded-full text-white"
-        href="https://gecken.co/"
+        href="https://srhart.co/"
         rel="noopener noreferrer"
         target="_blank"
       >
