abd99496f5bbd6fe82de86cc8e1a5fdfd665cee7
Andres Bedoya <andresbedoyag@Andress-MacBook-Pro.local>
2022-12-15 09:37:40 -0500

    Update import header to camelcase

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Footer from './Footer'
-import Header from './Header'
+import Header from './header'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
